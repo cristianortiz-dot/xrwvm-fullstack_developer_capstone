c351b9c711acd68fc98f00bbb0d08d4b73b4adbc
cristianortiz-dot <consultor.cristianortiz@gmail.com>
2026-08-11 17:34:03 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 
 from .models import CarMake, CarModel
 from .populate import initiate
